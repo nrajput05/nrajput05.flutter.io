500e27ca25e5b2659db697f866e994dbc61f17ca
Nipun Rajput <nrajput@IM1-KP6TXLH94L.local>
2025-02-04 15:56:00 +0530

    viewinset fix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "847c33ffedd067029755f9de632fcac8",
+const RESOURCES = {"flutter_bootstrap.js": "3d169df0c59c1fac03b5eb9ae0de3a22",
 "version.json": "80e48bcd8d00dc03ce06eca12dfb22b7",
 "index.html": "f14563597b315baa27a71376be669920",
 "/": "f14563597b315baa27a71376be669920",
-"main.dart.js": "48c7cc13bc44c4eb94195fdfff40f4f9",
+"main.dart.js": "f30e394673161038072273f01ac46268",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
